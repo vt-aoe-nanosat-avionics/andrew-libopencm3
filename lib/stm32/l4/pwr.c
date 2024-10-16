2239e38c25e3d25666779f65f8972b9a7d949ab6
andrewmcg1 <greekgeek191@gmail.com>
2024-10-16 04:44:04 -0400

    changed wfi to wfe for stop modes

--- lib/stm32/l4/pwr.c
+++ lib/stm32/l4/pwr.c
@@ -84,21 +84,21 @@ void pwr_enable_stop0_mode(void)
 {
 	scb_set_sleepdeep();
 	PWR_CR1 |= PWR_CR1_LPMS_STOP_0;
-	__asm__("wfi");
+	__asm__("wfe");
 }
 
 void pwr_enable_stop1_mode(void)
 {
 	scb_set_sleepdeep();
 	PWR_CR1 |= PWR_CR1_LPMS_STOP_1;
-	__asm__("wfi");
+	__asm__("wfe");
 }
 
 void pwr_enable_stop2_mode(void)
 {
 	scb_set_sleepdeep();
 	PWR_CR1 |= PWR_CR1_LPMS_STOP_2;
-	__asm__("wfi");
+	__asm__("wfe");
 }
 
 void pwr_enable_standby_mode(void)
@@ -117,6 +117,11 @@ void pwr_enable_shutdown_mode(void)
 {
 	scb_set_sleepdeep();
 	PWR_CR1 |= PWR_CR1_LPMS_SHUTDOWN;
+	PWR_SCR |= PWR_SCR_CWUF5;
+	PWR_SCR |= PWR_SCR_CWUF4;
+	PWR_SCR |= PWR_SCR_CWUF3;
+	PWR_SCR |= PWR_SCR_CWUF2;
+	PWR_SCR |= PWR_SCR_CWUF1;
 	__asm__("wfi");
 }
 /**@}*/
